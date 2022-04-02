301d4cdf22a9e00a7dd7a7d24cad9bf75b32ef2d
Mariana Nunes <marinun47@gmail.com>
2022-04-02 19:58:01 -0300

    Adiciona enunciados

---
+++ exemplo3.js
@@ -0,0 +1,2 @@
+Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+	a
---
+++ exemplo5.js
@@ -0,0 +1,1 @@
+Receba do usuários 3 números e calcule a média entre eles.

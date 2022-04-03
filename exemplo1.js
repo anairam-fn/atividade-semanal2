163113be4dbf3c7ec9dd743bf55700fbeb7ac31a
Mariana Nunes <marinun47@gmail.com>
2022-04-03 00:12:53 -0300

    Adiciona resposta do exemplo 1

--- exemplo1.js
+++ exemplo1.js
@@ -1,1 +1,11 @@
-Solicite o preço de uma mercadoria e o percentual de desconto 20%. Exiba no console o valor do desconto e o preço a pagar.
+// Solicite o preço de uma mercadoria e o percentual de desconto 20%.
+// Exiba no console o valor do desconto e o preço a pagar.
+
+const preçoMercadoria = 993;
+const desconto = preçoMercadoria * 0.2;
+
+const preçoPago = preçoMercadoria - desconto;
+
+//console.log("Com o desconto de R$" + desconto.toFixed(2) + ", o preço pago é R$" + preçoPago.toFixed(2) + ".")
+
+console.log(`Com o desconto de R$ ${desconto.toFixed(2)}, o valor pago é R$ ${preçoPago.toFixed(2)}`);

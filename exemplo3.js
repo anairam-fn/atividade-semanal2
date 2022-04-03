4bc65cd4649d59ac88cf6a2be786bc4e1c720cfb
Mariana Nunes <marinun47@gmail.com>
2022-04-03 00:14:48 -0300

    Adiciona resposta do exemplo 3

--- exemplo3.js
+++ exemplo3.js
@@ -1,2 +1,9 @@
-Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
-	a
+// Crie um algoritmo que leia o valor de um jantar, calcule e informe o
+// valor da taxa do garçom (10%) e o valor total a ser pago.
+
+const valorConta = 246.8;
+const taxaGarçom = (10 / 100) * valorConta;
+
+const valorTotal = valorConta + taxaGarçom;
+
+console.log(`O valor total da conta é R$ ${valorTotal.toFixed(2)}`);

82446f8715dba2e641e316cd292203c8f3d99068
Mariana Nunes <marinun47@gmail.com>
2022-04-03 00:15:42 -0300

    Adiciona resposta do exemplo 5

--- exemplo5.js
+++ exemplo5.js
@@ -1,1 +1,9 @@
-Receba do usuários 3 números e calcule a média entre eles.
+// Receba do usuários 3 números e calcule a média entre eles.
+
+const number1 = 3;
+const number2 = 10;
+const number3 = 8;
+
+const media = (number1 + number2 + number3) / 3;
+
+console.log(`A média é ${media}.`);

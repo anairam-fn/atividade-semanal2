868368c11e56d96d9beae8b04530d4ef837a4033
Mariana Nunes <marinun47@gmail.com>
2022-04-04 18:58:39 -0300

    Altera resposta do exemplo 2

--- exemplo2.js
+++ exemplo2.js
@@ -1,9 +1,18 @@
-// Calcule o tempo de uma viagem de carro. 
+// Calcule o tempo de uma viagem de carro.
 // Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
 
-const distanciaKm = 160;
+const distanciaKm = 100;
 const velocidadeKmH = 90;
 
-const tempoViagem = distanciaKm/velocidadeKmH
+const velocidadeKmMin = velocidadeKmH / 60;
+console.log(velocidadeKmMin);
 
-console.log(`O tempo da viagem será de ${tempoViagem.toFixed(1)} horas.`)
+const tempoViagemMinutos = distanciaKm / velocidadeKmMin;
+console.log(tempoViagemMinutos);
+
+const horas = Math.floor(tempoViagemMinutos / 60);
+const minutos = tempoViagemMinutos % 60;
+
+console.log(
+  `O tempo da viagem será de ${horas} horas e ${minutos.toFixed(0)} minutos.`
+);

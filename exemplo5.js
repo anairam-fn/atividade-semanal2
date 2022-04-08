d1dcf66878821febe4c3dd6c5bec456962441893
Mariana Nunes <marinun47@gmail.com>
2022-04-07 22:15:13 -0300

    Altera resposta do exemplo 5

--- exemplo5.js
+++ exemplo5.js
@@ -7,3 +7,9 @@ const number3 = 8;
 const media = (number1 + number2 + number3) / 3;
 
 console.log(`A média é ${media}.`);
+
+let distance = 500
+let velocity = 50
+let time = distance / velocity
+
+console.log(`A viagem irá durar cerca de ${time} horas.`)
